576c33d118e6bd570a112be935615ed53a36706d
FlqmeDevMC <Flameclient1.0@gmail.com>
2024-06-07 23:55:03 -0400

    Create features.h

---
+++ features.h
@@ -0,0 +1,18 @@
+// features.h
+#ifndef FEATURES_H
+#define FEATURES_H
+
+class Features {
+public:
+    Features();
+    ~Features();
+
+    void init();
+    void update();
+
+private:
+    // List of features (e.g. AimAssist, Reach, etc.)
+    //...
+};
+
+#endif // FEATURES_H
