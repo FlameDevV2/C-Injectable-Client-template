a2aa0bd6dc04c4c4b749e15014666daafd448a19
FlqmeDevMC <Flameclient1.0@gmail.com>
2024-06-07 23:54:41 -0400

    Create mappings.cpp

---
+++ mappings.cpp
@@ -0,0 +1,15 @@
+// mappings.cpp
+#include "mappings.h"
+
+Mappings::Mappings() {
+    mappings.clear();
+}
+
+Mappings::~Mappings() {
+    mappings.clear();
+}
+
+void Mappings::loadMappings(const std::string& version) {
+    // Load the mappings for the specified Minecraft version
+    //...
+}
